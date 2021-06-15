c37b8631004665f00c5abe5fab235b9d9d83a6b7
neozh3 <neozh3@users.noreply.github.com>
2021-06-15 09:33:49 +0800

    Update postMigrationChecks.py

--- Switch Migration Scripts/Post Migration/postMigrationChecks.py
+++ Switch Migration Scripts/Post Migration/postMigrationChecks.py
@@ -10,15 +10,19 @@ mySwitch = {
     'secret': 'cisco',     # optional, defaults to ''
 }
 
-#takes output of show int status, return a list of connected ports
+# takes output of show int status, return a list of connected ports
+
+
 def getConnectedPorts(shIntStatusOp):
     conPorts = []
     for iface in shIntStatusOp:
         if iface['status'] == 'connected':
             conPorts.append(iface['port'])
-    return conPorts #eg.['Gi0/0', 'Gi0/1', 'Gi0/2', 'Gi0/3']
+    return conPorts  # eg.['Gi0/0', 'Gi0/1', 'Gi0/2', 'Gi0/3']
 
-#takes a list of ports, run show interface {portnumber} and extract relevant values
+# takes a list of ports, run show interface {portnumber} and extract relevant values
+
+
 def getIfacecInfo(conPorts):
     '''This function prints out interfaces that have errors from show interface {portnumber}
         and returns a nested dict of values from the attr list
@@ -35,31 +39,39 @@ def getIfacecInfo(conPorts):
     '''
     ifaceValues = {}
 
-    #values of interest
-    #use this con.send_command("show int g0/1", use_textfsm=True) to test for available values
-    attr = ["address","crc","duplex","input_errors","input_packets","output_errors","output_packets","speed"]
+    # values of interest
+    # use this con.send_command("show int g0/1", use_textfsm=True) to test for available values
+    attr = ["address", "crc", "duplex", "input_errors",
+            "input_packets", "output_errors", "output_packets", "speed"]
 
-    #edit these 2 lists if there are new values to check
-    toCheckifZero = ["crc","input_errors","output_errors"]
+    # edit these 2 lists if there are new values to check
+    toCheckifZero = ["crc", "input_errors", "output_errors"]
     toCheckifNotZero = ["input_packets", "output_packets"]
 
-    #--clear counters and wait 5 seconds--
-    con.send_command("clear counters", expect_string=r"[confirm]")
-    con.send_command("\n")
-    time.sleep(5)
-    #-------------------------------------
+    print("####################################################")
+    print("# Checking for interface errors on connected ports #")
+    print("####################################################")
+    print("\n")
+
+    clearCntInput = input(r"Do you want to clear counters [y/n]: ")
+    print("\n")
+    if clearCntInput == 'y':
+        # --clear counters and wait 5 seconds--
+        con.send_command("clear counters", expect_string=r"[confirm]")
+        con.send_command("\n")
+        time.sleep(5)
+        # -------------------------------------
 
     for port in conPorts:
-        #--- to get attributes of interest from show show int {portnumber} ---
+        # --- to get attributes of interest from show show int {portnumber} ---
         command = f"show interface {port}"
         op = con.send_command(command, use_textfsm=True)
-        #get iface number eg. GigabitEthernet0/0 to use as dict key
+        # get iface number eg. GigabitEthernet0/0 to use as dict key
         iface = op[0]["interface"]
-        #only keep keys that is in attr list
-        ifaceValues[iface]  = { v: op[0][v] for v in attr }
+        # only keep keys that is in attr list
+        ifaceValues[iface] = {v: op[0][v] for v in attr}
 
-
-        #--- to check for errors in the interface by checking values of crc errors etc
+        # --- to check for errors in the interface by checking values of crc errors etc
         for value in toCheckifZero:
             contChecking = True
             if op[0][value] != '0':
@@ -75,8 +87,8 @@ def getIfacecInfo(conPorts):
                     print(f"{iface} interface is ok")
                     break
 
-
     return ifaceValues
+
 
 def checkArpForIp(ip):
     op = con.send_command(f"show ip arp | in {ip}", use_textfsm=True)
@@ -93,30 +105,26 @@ def checkArpForIp(ip):
             print(f"ERROR - Unable to arp for gateway {ip}")
 
 
-
-
-
 if __name__ == "__main__":
 
     cmd = ["show int status", "show mac address-table dynamic"]
 
     with ConnectHandler(**mySwitch) as con:
-        con.disable_paging() #term len 0
-        con.enable()         #enable
+        con.disable_paging()  # term len 0
+        con.enable()  # enable
         #prompt = con.find_prompt() #get the prompt eg. ESW01#
 
-        #Dict with the key as command entered, value as output
-        #eg. to access show int status output -> cmdOutput['show int status']
+        # Dict with the key as command entered, value as output
+        # eg. to access show int status output -> cmdOutput['show int status']
         cmdOutput = {}
         for c in cmd:
-            #print(c)
+            # print(c)
             op = con.send_command(c, use_textfsm=True)
             cmdOutput[c] = op
 
-
         shIntStatusOp = cmdOutput['show int status']
         ###example value###
-        #[{'duplex': 'a-full',
+        # [{'duplex': 'a-full',
         #  'fc_mode': '',
         #  'name': '',
         #  'port': 'Gi0/0',
@@ -125,10 +133,9 @@ if __name__ == "__main__":
         #  'type': 'RJ45',
         #  'vlan': 'routed'}, {..Next dict..}]
 
-
         shMacTableOp = cmdOutput['show mac address-table dynamic']
         ###exampe value###
-        #[{'destination_address': '2200.0001.0002',
+        # [{'destination_address': '2200.0001.0002',
         #  'destination_port': 'Gi0/2',
         #  'type': 'DYNAMIC',
         #  'vlan': '1'},
@@ -137,20 +144,30 @@ if __name__ == "__main__":
         #  'type': 'DYNAMIC',
         #  'vlan': '1'}]
 
-
         connectedPorts = getConnectedPorts(shIntStatusOp)
 
         ifaceInfo = getIfacecInfo(connectedPorts)
-        #print(ifacecInfo.keys())
+        # print(ifacecInfo.keys())
+        
+        print("\n")
+        gwInput = input(r"Is there gateway configured [y/n]: ")
+        print("\n")
 
-        gateway = con.send_command(r"show run | in gateway")
-        #assume that show run | in gateway output is - ip default-gateway {ipaddr}
-        #slice first part of string to only get the ip address
-        #sometimes slice not working?? sometimes there will be another space infront of ip
-        gateway = gateway[19:]
-        gateway = gateway.lstrip()
+        if gwInput == 'y':
 
-        #ping gateway to trigger arp request
-        con.send_command(r"ping {gateway}")
+            print("####################################")
+            print("# Checking if gateway is reachable #")
+            print("####################################")
+            print("\n")
+            gateway = con.send_command(r"show run | in gateway")
+            # assume that show run | in gateway output is - ip default-gateway {ipaddr}
+            # slice first part of string to only get the ip address
+            # sometimes slice not working?? sometimes there will be another space infront of ip
+            gateway = gateway[19:]
+            gateway = gateway.lstrip()
 
-        checkArpForIp(gateway)
+            # ping gateway to trigger arp request
+            con.send_command(r"ping {gateway}")
+
+            checkArpForIp(gateway)
+        print("--------Script Completed--------")
