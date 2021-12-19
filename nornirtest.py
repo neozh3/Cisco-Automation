93ef5403822c7d7962fe576fd313186ac0e0d4b0
neozh3 <neozh3@users.noreply.github.com>
2021-12-19 21:25:42 +0800

    Delete nornirtest.py

--- nornirtest.py
+++
@@ -1,9 +0,0 @@
-from nornir import InitNornir
-from nornir_utils.plugins.functions import print_result
-from nornir_netmiko import netmiko_send_command
-
-nr = InitNornir(config_file="config.yaml")
-
-res = nr.run(task=netmiko_send_command,command_string="wr")
-
-print_result(res)
