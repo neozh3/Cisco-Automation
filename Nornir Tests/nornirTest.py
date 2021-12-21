8f9c2b5bc76663e32c3f68e3ca00c72615db7254
neozh3 <neozh3@users.noreply.github.com>
2021-12-21 10:52:06 +0800

    Update nornirTest.py

--- Nornir Tests/nornirTest.py
+++ Nornir Tests/nornirTest.py
@@ -1,9 +1,18 @@
 from nornir import InitNornir
+from nornir.core.inventory import Group
 from nornir_utils.plugins.functions import print_result
+from nornir_utils.plugins.tasks.files import write_file
 from nornir_netmiko import netmiko_send_command
+from nornir.core.filter import F
 
 nr = InitNornir(config_file="config.yaml")
 
-res = nr.run(task=netmiko_send_command,command_string="wr")
 
+#routers = nr.filter(F(groups__contains="local_grp"))
+#res = routers.run(task=netmiko_send_command,command_string="show ip int br | ex unas")
+res = nr.run(task=netmiko_send_command,command_string="show ip int br | ex unas")
 print_result(res)
+
+
+#cli output
+#a = res["R1"][0].result
